a77da86ea5fcf7d6c8ca52201498f0a43fd835a6
hieplt1018 <hieplt1018@gmail.com>
2023-04-16 23:51:36 +0700

    fix-cookie-not-saved-7

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ dotenv.config({ path: 'server/config/config.env' });
 
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cookieParser(config.cookiesKey));
+app.use(cookieParser());
 app.use(cors({
   origin: true,
   credentials: true
