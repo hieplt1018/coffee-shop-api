a145e8b0f98d05f41f42e1dfd22e207f4384f147
hieplt1018 <hieplt1018@gmail.com>
2023-04-17 00:17:55 +0700

    fix-cookie-not-saved-12

--- server/app.js
+++ server/app.js
@@ -10,10 +10,7 @@ const cors = require('cors');
 
 dotenv.config({ path: 'server/config/config.env' }); 
 
-app.use(cors({
-  origin: "https://coffee-shop-api-xycm.onrender.com",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
