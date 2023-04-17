9c4ab63e25abe79dbdc915bf09bb88bec030c0d3
hieplt1018 <hieplt1018@gmail.com>
2023-04-17 07:43:51 +0700

    fix-cookie-not-saved-13

--- server/app.js
+++ server/app.js
@@ -10,7 +10,14 @@ const cors = require('cors');
 
 dotenv.config({ path: 'server/config/config.env' }); 
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+  sameSite: none,
+  allowedHeaders: ['Content-Type', 'x-auth-token'],
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: 'https://cantata-coffee.onrender.com'
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
